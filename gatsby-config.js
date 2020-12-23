e28b8099e2f5d734f21f23e37e15f8df25272ce2
Nobuyuki Hiwatashi <hiwanobu@gmail.com>
2020-12-23 21:53:53 +0900

    add trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-6HYGS76WKK`,
       },
     },
     `gatsby-plugin-feed`,
